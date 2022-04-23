212fc6d1a37ae19843b9dff366098bc709d19b8c
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-04-24 00:39:41 +1000

    Build SkaterPunks contract with constructor and minting functions

--- contracts/CollectionNFT.sol
+++ contracts/CollectionNFT.sol
@@ -3,3 +3,38 @@ pragma solidity ^0.8.4;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract CollectionNFT is ERC721A, Ownable {
+
+    uint256 public cost;
+    uint256 public maxSupply;
+
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256 _cost,
+        uint256 _maxSupply
+    ) ERC721A(_tokenName, _tokenSymbol) {
+        cost = _cost;
+        maxSupply = _maxSupply;
+    }
+
+    /// @notice Check that the mint amount is valid
+    modifier mintCompliance(uint256 _mintAmount) {
+        require(_mintAmount > 0, "CollectionNFT: Invalid mint amount");
+        require(totalSupply() + _mintAmount <= maxSupply, "CollectionNFT: Max supply exceeded");
+        _;
+    }
+
+    /// @notice Check that the payment amount is sufficient
+    modifier mintPriceCompliance(uint256 _mintAmount) {
+        require(msg.value >= cost * _mintAmount, "CollectionNFT: Insufficient funds");
+        _;
+    }
+
+    /// @notice Mint `_mintAmount` tokens for `msg.sender`
+    function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+        _safeMint(_msgSender(), _mintAmount);
+    }
+}
