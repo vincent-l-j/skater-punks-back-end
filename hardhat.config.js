d8d0b8d258392eed5df579d80f10e5f65f04d6f4
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-04-25 11:23:55 +1000

    Add api keys to .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv")
+
+dotenv.config();
 
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
